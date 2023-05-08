d17a5453294befcbf0f5349430879b71e6292c39
federey99 <federicogabrielrey@gmail.com>
2023-05-07 22:07:48 -0300

    Estructura del CV terminada

--- cv_api/src/components/footer/Footer.jsx
+++ cv_api/src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
+
 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer id='footer'>
+    <a href="#" className='footer__logo'>FEDERICO REY</a>
+
+    <ul className='permalinks'>
+      <li> <a href="#">Home</a> </li>
+      <li> <a href="#about">Sobre Mi</a> </li>
+      <li> <a href="#experience">Experiencia</a> </li>
+      <li> <a href="#services">Servicios</a> </li>
+      <li> <a href="#portfolio">Portfolio</a> </li>
+      <li> <a href="#testimonials">Referencias</a> </li>
+      <li> <a href="#contact">Contacto</a> </li>
+    </ul>
+
+    <div className="footer__socials">
+      <a href="https://facebook.com"><FaFacebookF/></a>
+      <a href="https://instagram.com"><FiInstagram/></a>
+      <a href="https://twitter.com"><IoLogoTwitter/></a>
+    </div>
+
+    <div className="footer__copyright">
+      <small>&copy; Proyectos de Federico Rey. Todos los derechos reservados.</small>
+    </div>
+    </footer>
   )
 }
 
