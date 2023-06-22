9f03c9db3d7aff0c0ecf9fa1e14c12c5c1cc1443
federey99 <federicogabrielrey@gmail.com>
2023-06-21 23:29:14 -0300

    se agregan los contactos

--- backend/src/routes/contactos.routes.js
+++ backend/src/routes/contactos.routes.js
@@ -3,11 +3,30 @@ const {check} = require("express-validator");
 const router = express.Router();
 const checkFields = require("../middlewares/validateFields");
 const contactosController = require('../controllers/contactos.controller');
+const jwtValidator = require('../middlewares/jwtValidator');
 
 //const router = Router();
 
-//router.get("/",jwtValidator,contactosController.getContactos);//GET CONTACTOS
+router.get('/',
+[
+    check('jwt').not().isEmpty(),
+    checkFields
+],
+jwtValidator,contactosController.getContactos);//GET CONTACTOS
 
-//router.get("/:id",jwtValidator,contactosController.getContactosByID);//GET CONTACTOS BY ID
+router.get('/:id',[
+    check('jwt').not().isEmpty(),
+    checkFields
+],jwtValidator,contactosController.getContactosByID);//GET CONTACTOS BY ID
+
+router.post('/',[ // POST CONTACTOS
+    //check('jwt').not().isEmpty(),
+    check('Nombre').not().isEmpty(),
+    check('Apellido').not().isEmpty(),
+    check('Domicilio').not().isEmpty(),
+    check('Mail').not().isEmpty(),
+    check('Mensaje').not().isEmpty(),
+    checkFields
+],jwtValidator,contactosController.createContacto);
 
 module.exports = router;
--- backend/src/routes/usuarios.routes.js
+++ backend/src/routes/usuarios.routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const usuariosController = require('../controllers/usuarios.controller');
 const checkFields = require("../middlewares/validateFields");
 
-//const router = Router();
 
 router.get("/",usuariosController.getUsuarios);//GET usuarios
 router.get("/:id",usuariosController.getUsuariosByID);//GET usuarios BY ID
